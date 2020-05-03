10bfae795105baa10b3b0b810f0edff916aec1ea
Willi Mentzel <willi.mentzel@gmail.com>
2020-05-03 15:46:04 +0200

    added HTML builder as found on try.kotlinlang.org

--- src/main/kotlin/com/randomlychosenbytes/kotlincv/Main.kt
+++ src/main/kotlin/com/randomlychosenbytes/kotlincv/Main.kt
@@ -1,4 +1,35 @@
 package com.randomlychosenbytes.kotlincv
 
 fun main() {
+    val result =
+            html {
+                head {
+                    title { +"HTML encoding with Kotlin" }
+                }
+                body {
+                    h1 { +"HTML encoding with Kotlin" }
+                    p { +"this format can be used as an alternative markup to HTML" }
+
+                    // an element with attributes and text content
+                    a(href = "http://jetbrains.com/kotlin") { +"Kotlin" }
+
+                    // mixed content
+                    p {
+                        +"This is some"
+                        b { +"mixed" }
+                        +"text. For more see the"
+                        a(href = "http://jetbrains.com/kotlin") { +"Kotlin" }
+                        +"project"
+                    }
+                    p { +"some text" }
+
+                    // content generated from command-line arguments
+                    p {
+                        +"Command line arguments were:"
+                        ul {
+                        }
+                    }
+                }
+            }
+    println(result)
 }
