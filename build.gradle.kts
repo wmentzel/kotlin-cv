b1474d80ed0ad5ded05c84659a41df11c73b873a
Willi Mentzel <willi.mentzel@gmail.com>
2026-01-06 20:20:15 +0100

    Bump Gradle and Kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,20 +1,34 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     application
-    kotlin("jvm") version "1.3.71"
+    kotlin("jvm") version "2.0.21"
 }
 
 application {
-    mainClassName = "com.randomlychosenbytes.kotlincv.MainKt"
+    mainClass.set("com.randomlychosenbytes.kotlincv.MainKt")
 }
 
 dependencies {
-    compile(kotlin("stdlib"))
-    compile("com.itextpdf.tool:xmlworker:5.5.13.1")
-    compile("com.itextpdf:itextpdf:5.5.13.1")
+    implementation(kotlin("stdlib"))
+    implementation("com.itextpdf.tool:xmlworker:5.5.13.1")
+    implementation("com.itextpdf:itextpdf:5.5.13.1")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.5")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.5")
 }
 
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(22))
+    }
+}
+
+tasks.withType<KotlinCompile>().configureEach {
+    compilerOptions {
+        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_22)
+    }
+}
+
 repositories {
-    jcenter()
+    mavenCentral()
 }
